17d9a075a3df4f69adf6df1498dc51f5fdc08e8d
Norbert Marwan <norbert.marwan@gmail.com>
2021-02-07 01:26:21 +0100

    update tests

--- tests/test_rp_iso.m
+++ tests/test_rp_iso.m
@@ -1,8 +1,8 @@
 %% test rp.m
 
-x = load('data/noise_embed.dat');
-rp_test_iso = load('data/noise_rpiso.dat');
-rp_test_perp = load('data/noise_rpperp.dat');
+x = load('tests/data/noise_embed.dat');
+rp_test_iso = load('tests/data/noise_rpiso.dat');
+rp_test_perp = load('tests/data/noise_rpperp.dat');
 
 r = rp_iso(x,.2,.3);
 assert(isequal(rp_test_iso,r))
